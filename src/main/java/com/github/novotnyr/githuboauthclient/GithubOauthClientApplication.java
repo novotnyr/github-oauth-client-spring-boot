e62467a5e2bd868d6b340fd1fb49d64018683355
Robert Novotny <novotnr0@gmail.com>
2023-04-01 21:54:30 +0200

    Initial import

---
+++ src/main/java/com/github/novotnyr/githuboauthclient/GithubOauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.novotnyr.githuboauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubOauthClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubOauthClientApplication.class, args);
+    }
+
+}

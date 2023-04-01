1dd3d9fdf64965049f9a1ef8cddd6f98c2b6ce27
Robert Novotny <novotnr0@gmail.com>
2023-04-02 00:06:28 +0200

    Add a sample endpoint that proxies GitHub

--- src/main/java/com/github/novotnyr/githuboauthclient/ApiController.java
+++ src/main/java/com/github/novotnyr/githuboauthclient/ApiController.java
@@ -1,9 +1,14 @@
 package com.github.novotnyr.githuboauthclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,5 +21,17 @@ public class ApiController {
     @GetMapping("/")
     public Map<String, String> getVersion() {
         return Map.of("version", "1.0");
+    }
+
+    @GetMapping("/repositories")
+    public List<Repository> getRepositories() {
+        String url = "https://api.github.com/user/repos?type=owner&since=2023-01-01T00:00:00Z";
+        var repositoryListType = new ParameterizedTypeReference<List<Repository>>() {
+        };
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(repositoryListType)
+                .block();
     }
 }
